cf58f55ece66523be68c7c0a678c2b0082d8033f
“karandepale” <karandepale111@gmail.com>
2023-05-25 13:00:04 -0700

    Add:As a fan of geometry, I want to model a line based on a point consisting of (x, y) co
    -ordinates
    using the Cartesian system,
    So that I can calculate its
    length. - A Length as 2 Points (x1, y1) and (x2, y2) - Length of a Line = sqrt( (x2 - x1) ^ 2 + (y2
    
    - y1) ^ 2)

--- OOPSLCP/Program.cs
+++ OOPSLCP/Program.cs
@@ -1,16 +1,62 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OOPSLCP
 {
+    internal class Point
+    {
+        private int x;
+        private int y;
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int X
+        {
+            get { return x; }
+        }
+
+        public int Y
+        {
+            get { return y; }
+        }
+    }
+
+    internal class Line
+    {
+        private Point point1;
+        private Point point2;
+
+        public Line(Point point1, Point point2)
+        {
+            this.point1 = point1;
+            this.point2 = point2;
+        }
+
+        public double CalculateLength()
+        {
+            int xDiff = point2.X - point1.X;
+            int yDiff = point2.Y - point1.Y;
+            double length = Math.Sqrt(Math.Pow(xDiff, 2) + Math.Pow(yDiff, 2));
+            return length;
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Line Comparision Program using Object oriented programming...");
+            Console.WriteLine("Welcome to Line Comparison Program using Object-oriented programming...");
+
+            Point point1 = new Point(3, 4);
+            Point point2 = new Point(6, 8);
+
+            Line line = new Line(point1, point2);
+            double length = line.CalculateLength();
+
+            Console.WriteLine($"Length of the line: {length}");
         }
     }
 }
